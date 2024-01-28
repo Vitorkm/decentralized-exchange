0a294f3cd0249d1b89d9646130d4276e61bec260
Vitor Kretiska <vitor-kretiska@hotmail.com>
2024-01-28 12:14:00 -0300

    added swap frontend

--- frontend/src/components/Swap.js
+++ frontend/src/components/Swap.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Input, Popover, Radio, Modal, message } from "antd";
+import {
+  ArrowDownOutlined,
+  DownOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import tokenlist from "../tokenList.json";
 
 function Swap() {
+  const [slippage, setSlippage] = useState(2.5);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne] = useState(tokenlist[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenlist[11]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
+
+  function handleSlippageChange(e) {
+    setSlippage(e.target.value);
+  }
+
+  function changeAmount(e) {
+    setTokenOneAmount(e.target.value);
+  }
+
+  function switchTokens() {
+    setTokenOne(tokenTwo);
+    setTokenTwo(tokenOne);
+  }
+
+  function openModal(asset) {
+    setChangeToken(asset);
+    setIsOpen(true);
+  }
+
+  function modifyToken(index) {
+    if (changeToken === 1) {
+      setTokenOne(tokenlist[index]);
+    } else {
+      setTokenTwo(tokenlist[index]);
+    }
+    setIsOpen(false);
+  }
+
+  const settings = (
+    <>
+      <div>Slippage Tolerance</div>
+      <div>
+        <Radio.Group value={slippage} onChange={handleSlippageChange}>
+          <Radio.Button value={0.5}>0.5%</Radio.Button>
+          <Radio.Button value={2.5}>2.5%</Radio.Button>
+          <Radio.Button value={5}>5.0%</Radio.Button>
+        </Radio.Group>
+      </div>
+    </>
+  );
+
   return (
-    <div>Swap</div>
-  )
+    <>
+      <Modal
+        open={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+        title="Select a token"
+      >
+        <div className="modalContent">
+          {tokenlist?.map((token, index) => {
+            return (
+              <div
+                className="tokenChoice"
+                key={index}
+                onClick={() => modifyToken(index)}
+                >
+                  <img src={token.img} alt={token.ticker} className="tokenLogo" />
+                  <div className="tokenChoiceNames">
+                    <div className="tokenName">{token.name}</div>
+                    <div className="tokenTicker">{token.ticker}</div>
+                  </div>
+                </div>
+            );
+          })}
+        </div>
+      </Modal>
+      <div className="tradeBox">
+        <div className="tradeBoxHeader">
+          <h4>Swap</h4>
+          <Popover
+            title="Settings"
+            trigger="click"
+            placement="bottomRight"
+            content={settings}
+          >
+            <SettingOutlined className="cog" />
+          </Popover>
+        </div>
+        <div className="inputs">
+          <Input
+            placeholder="0"
+            value={tokenOneAmount}
+            onChange={changeAmount}
+          />
+          <Input placeholder="0" value={tokenTwoAmount} disabled />
+          <div className="switchButton" onClick={switchTokens}>
+            <ArrowDownOutlined className="switchArrow" />
+          </div>
+          <div className="assetOne" onClick={() => openModal(1)}>
+            <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+            {tokenOne.ticker}
+            <DownOutlined />
+          </div>
+          <div className="assetTwo" onClick={() => openModal(2)}>
+            <img src={tokenTwo.img} alt="assetTwoLogo" className="assetLogo" />
+            {tokenTwo.ticker}
+            <DownOutlined />
+          </div>
+        </div>
+        <div className="swapButton" disabled={!tokenOneAmount}>Swap</div>
+      </div>
+    </>
+  );
 }
 
-export default Swap
+export default Swap;
